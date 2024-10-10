4cbd5a54cd505614e09ea099c604c0ac311b7734
ArianTrovato <fedetrovato21@gmail.com>
2024-10-10 13:09:57 -0300

    cambio de mauri 3

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -8,9 +8,10 @@ import { Users } from 'src/users/users.entity';
 import { Products } from 'src/Products/products.entity';
 import { Cart } from 'src/cart/cart.entity';
 import { CartService } from 'src/cart/cart.service';
+import { Categories } from 'src/categories/categories.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderDetails, Orders, Users, Products, Cart])],
+  imports: [TypeOrmModule.forFeature([OrderDetails, Orders, Users, Products, Cart, Categories])],
   controllers: [OrdersController],
   providers: [OrdersService, CartService],
 })
